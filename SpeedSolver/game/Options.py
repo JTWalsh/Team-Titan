59efa80503b5b51ab5a561fb7decbc0421de4623
bloodruns4ever <bloodruns4ever@gmail.com>
2014-04-21 23:40:21 -0400

    Commented a majority of the code
    Options now display 4 operand check boxes. Difficulty will be a scene
    before the game starts.

--- SpeedSolver/game/Options.py
+++ SpeedSolver/game/Options.py
@@ -5,34 +5,45 @@ import MainScreen
 
 WIDTH = 1200
 HEIGHT = 900
-BG_COLOR = (0,0,0)
-WHITE = (255, 255, 255)
 SIZE = (WIDTH, HEIGHT)
-DEF_FONT = "libraries/spyral/resources/fonts/DejaVuSans.ttf"
 
 
 class OptionScene(spyral.Scene):
     def __init__(self):
         super(OptionScene, self).__init__(SIZE)
 
+#style that allows custom checkboxes
         self.load_style("game/style.spys")
 
+#Quit button or escape stops the game
         spyral.event.register('input.keyboard.down.esc', spyral.director.quit)
         spyral.event.register("system.quit", spyral.director.quit)
 
+#Option Scene Background
         self.background = spyral.Image("images/Option_Menu.png")
 
 
+#Generates the Operand Checkboxes
         class RegisterForm(spyral.Form):
             BackButton = spyral.widgets.Button("Go Back")
-            EasyBox = spyral.widgets.Checkbox()
+            AdditionBox = spyral.widgets.Checkbox()
+            SubtractionBox = spyral.widgets.Checkbox()
+            MultiplicationBox = spyral.widgets.Checkbox()
+            DivisionBox = spyral.widgets.Checkbox()
 
         my_form = RegisterForm(self)
         my_form.focus()
 
-        my_form.EasyBox.pos = (300, 300)
+
+#Sets the Checkbox positions
+        my_form.AdditionBox.pos = (50, 450)
+        my_form.SubtractionBox.pos = (50, 650)
+        my_form.MultiplicationBox.pos = (600, 450)
+        my_form.DivisionBox.pos = (600, 650)
         my_form.BackButton.pos = (1100, 800)
 
+
+#Goes back to the main menu screen when button is clicked
         spyral.event.register("form.RegisterForm.BackButton.clicked", self.goToMenu)
 	
     def goToMenu(self):
